258660d0594f8c584320579eee9f58840c1ef0b5
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-12-01 15:47:04 +0100

    Write tests for translate

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,36 @@ pub mod helpers {
             .map(|chunk| String::from_utf8(chunk.to_vec()).unwrap())
             .collect()
     }
+    pub fn translate(sequence: &Vec<String>) -> String {
+        let mut peptide = String::new();
+        for codon in sequence {
+            peptide.push_str(match codon.as_str() {
+                "gcg" | "gcc" | "gca" | "gct" => "A",
+                "cgu" | "cgc" | "cga" | "cgg" | "aga" | "agg" => "R",
+                "aau" | "aac" => "N",
+                "gau" | "gac" => "D",
+                "ugu" | "ugc" => "C",
+                "caa" | "cag" => "Q",
+                "gaa" | "gag" => "E",
+                "ggu" | "ggc" | "gga" | "ggg" => "G",
+                "cau" | "cac" => "H",
+                "auu" | "auc" | "aua" => "I",
+                "cuu" | "cuc" | "cua" | "cug" | "uua" | "uug" => "L",
+                "aaa" | "aag" => "K",
+                "aug" => "M",
+                "uuu" | "uuc" => "F",
+                "ccu" | "ccc" | "cca" | "ccg" => "P",
+                "ucu" | "ucc" | "uca" | "ucg" | "agu" | "agc" => "S",
+                "acu" | "acc" | "aca" | "acg" => "T",
+                "ugg" => "W",
+                "uau" | "uac" => "Y",
+                "guu" | "guc" | "gua" | "gug" => "V",
+                "uaa" | "uag" | "uga" => "-",
+                _ => "X",
+            });
+        }
+        peptide
+    }
 }
 
 #[cfg(test)]
@@ -27,5 +57,23 @@ mod tests {
         let random_sequence = String::from_utf8(vec![b'a'; 100]).unwrap();
         let splitted = split_sequence(&random_sequence);
         assert_eq!(splitted.len(), 34);
+    }
+
+    #[test]
+    fn should_translate() {
+        let sequence = vec![
+            String::from("aug"),
+            String::from("ugg"),
+            String::from("aag"),
+            String::from("aug"),
+            String::from("ugg"),
+            String::from("aag"),
+            String::from("aug"),
+            String::from("ugg"),
+            String::from("aag"),
+        ];
+        let peptide = translate(&sequence);
+        assert_ne!(peptide, "MMM");
+        assert_eq!(peptide, "MWKMWKMWK");
     }
 }
